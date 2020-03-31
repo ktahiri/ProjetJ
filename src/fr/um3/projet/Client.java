78a030f5fee4bef6abf97e8906c9e46bab4a141c
tahir <tahir@192.168.1.101>
2020-03-31 18:21:56 +0200

    Khalid TAHIRI: Realisation de la classe Client

---
+++ src/fr/um3/projet/Client.java
@@ -0,0 +1,35 @@
+package fr.um3.projet;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.*;
+public class Client {
+
+	public static void main(String[] args) {
+		System.out.println("----Client----");
+		
+		try {
+			InetAddress add= InetAddress.getLocalHost();
+			Socket SocketClient = new Socket("192.168.1.101",49874);
+			String id="Client:  "+add;
+			
+			PrintWriter Envoie=new PrintWriter(SocketClient.getOutputStream());
+			Envoie.println(id);
+			Envoie.println("Bonjours");
+			
+			Envoie.flush();
+			
+			SocketClient.close();
+		
+		
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+
+	}
+
+}
